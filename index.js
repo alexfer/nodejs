09707dbb50ba8ad169f2c3ef0810ec33ff28a1dc
Aleksander Sh <aleksandersh@MacBook-Air-Aleksander.local>
2024-08-04 13:21:34 +0300

    update src

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
-const port = process.env.PORT || 3000;
+let keys = {};
+let cursor = '0';
+const redis = require('redis');
+const port = process.env.WEBSOCKET_PORT || 3000;
+const client = redis.createClient({
+    socket: {
+        path: process.env.REDIS_PATH
+    }
+});
+const scan = async (match = '*') => {
+    await client.connect();
+    console.log('Connected to Redis server...');
+    await client.keys('rgfly_*', function (err, k) {
+        if (err) return console.log(err);
+
+        for(let i = 0, len = k.length; i < len; i++) {
+            console.log(k[i]);
+            keys[k[i]] = k[i];
+        }
+    });
+    await client.quit();
+    console.log('Disconnected!');
+    return keys;
+}
 
 const WebSocketServer = require('ws').Server
     , wss = new WebSocketServer({port: port});
@@ -6,9 +29,10 @@ const WebSocketServer = require('ws').Server
 console.log('Server started on port: ' + port);
 
 wss.on('connection', function (ws) {
-    console.log('Connection started');
     ws.on('message', function (message) {
-        console.log('Received from client: %s', message);
-        ws.send('Server received from client: ' + message);
+        scan('mess_').then((result, keys) => {
+            console.log(result, keys);
+        });
+        ws.send('Websocket server received from client: ' + message);
     });
 });
