5b37dbf9b022f71cc8b0d205ae17a095712144e8
Aleksander Sh <aleksandersh@MacBook-Air-Aleksander.local>
2024-08-12 09:04:29 +0300

    decript hash

---
+++ decryptor.js
@@ -0,0 +1,15 @@
+import crypto from 'crypto';
+
+function decrypt(text = '', algo, key) {
+    const textParts = text.split(':');
+    let iv = Buffer.from(textParts.shift(), 'hex');
+    let encryptedText = Buffer.from(textParts.join(':'), 'hex');
+    let decipher = crypto.createDecipheriv(algo, Buffer.from(key), iv);
+    let decrypted = decipher.update(encryptedText);
+
+    decrypted = Buffer.concat([decrypted, decipher.final()]);
+
+    return decrypted.toString();
+}
+
+export default decrypt;
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import {v4 as uuidv4} from 'uuid';
 import {WebSocketServer} from 'ws';
 import scanAll from './scanner.js';
 import {Base64} from 'js-base64';
+import decrypt from "./decryptor.js";
 
 const port = process.env.WEBSOCKET_PORT;
 
@@ -24,6 +25,7 @@ wss.on('connection', (ws) => {
         const data = JSON.parse(messageAsString);
 
         data.hash = Base64.decode(data.hash);
+        data.hash = decrypt(data.hash, process.env.ALGORITHM, process.env.SECRET_KEY);
         const metadata = clients.get(ws);
         data.sender = metadata.id;
 
