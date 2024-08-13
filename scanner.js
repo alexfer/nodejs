f4a4546a9542796673ab17eef99397c47a9cc9cd
Aleksander Sh <aleksandersh@MacBook-Air-Aleksander.local>
2024-08-13 08:36:50 +0300

    minor changes

--- scanner.js
+++ scanner.js
@@ -1,12 +1,27 @@
 import redis from 'redis';
 
 const redisClient = redis.createClient({
+    connectTimeout: 10000,
     socket: {
-        path: process.env.REDIS_PATH
+        path: process.env.REDIS_PATH,
+        reconnectStrategy: function(retries) {
+            if (retries > 20) {
+                console.log("Too many attempts to reconnect. Redis connection was terminated");
+                return new Error("Too many retries.");
+            } else {
+                return retries * 500;
+            }
+        }
     }
 })
     .on('error', err => console.log('Redis Client Error', err))
     .connect();
+
+// const redisClient = redis.createClient({
+//     url: process.env.REDIS_URL,
+// })
+//     .on('error', err => console.log('Redis Client Error', err))
+//     .connect();
 
 const scanAll = async (pattern = '*', count = 100) => {
     const results = {};
